59ff412aad565814293de852659c1983b1a4348e
Billy Chui <billy2022002@gmail.com>
2022-04-18 11:55:22 +1000

    connect apollo client

---
+++ src/ApolloProvider.jsx
@@ -0,0 +1,23 @@
+import {
+	ApolloClient,
+	ApolloProvider,
+	InMemoryCache,
+	createHttpLink,
+} from '@apollo/client';
+import App from './App';
+
+const httpLink = createHttpLink({
+	uri: 'http://localhost:4000/',
+});
+
+const client = new ApolloClient({
+	link: httpLink,
+	cache: new InMemoryCache(),
+});
+
+const Apollo = () => (
+	<ApolloProvider client={client}>
+		<App />
+	</ApolloProvider>
+);
+export default Apollo;
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.module.css';
+
+function App() {
+	return (
+		<div>
+			<h1>Hello World!!</h1>
+		</div>
+	);
+}
+
+export default App;
